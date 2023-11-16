6d7252720e303bbb878e92086cf36bfa3038d012
Alimurz <148546039+Alimurz@users.noreply.github.com>
2023-11-16 17:29:05 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,58 +1,32 @@
-const Jokes = async () => {
-    try {
-        const resp = await fetch('https://official-joke-api.appspot.com/random_joke');
-        const data = await resp.json();
-
-        const targetId = document.getElementById('id');
-        const types = document.getElementById('type');
-        const setups = document.getElementById('setup');
-        const punchline = document.getElementById('punchline');
-
-        if (targetId) {
-            targetId.src = data.id;
-            console.log(targetId);
-            targetId.textContent = data.id;
-            return;
-        }else if (types) {
-            types.src = data.type;
-            types.textContent = data.type;
-            return;
-        }else if (setups) {
-            setups.src = data.setup;s
-            setups.textContent = data.setup;
-            return;
-        }else if (punchline) {
-            punchline.src = data.punchline;
-            punchline.textContent = data.punchline;
-            return;
-        } 
-
-        const id = document.createElement('h2');
-        const type = document.createElement('span');
-        const setup = document.createElement('p');
-        const text = document.createElement('span');
-       
-        text.id = 'punchline';
-        id.id = 'id';
-        setup.id = 'setup';
-        type.id = 'type';
-
-        id.textContent = data.id;
-        type.textContent = data.type;
-        setup.textContent = data.setup;
-        text.textContent = data.punchline;
-
-        const jkDiv = document.querySelector('.JK');
-        jkDiv.appendChild(id);
-        jkDiv.appendChild(type);
-        jkDiv.appendChild(setup);
-        jkDiv.appendChild(text);
-
-        console.log(data);
-    } catch (e) {
-        console.log(e, 'oops something went wrongююю');
-    }
-}
 document.addEventListener("DOMContentLoaded", () => {
-    Jokes();
+  Jokes();
 })    
+const Jokes = async () => {
+
+  try {
+      const resp = await fetch('https://official-joke-api.appspot.com/random_joke');
+      const data = await resp.json();
+
+      updateElement('data-id', 'h2', data.id)
+      updateElement('data-type', 'span', data.type)
+      updateElement('data-setup', 'p', data.setup)
+      updateElement('data-punchline', 'span', data.punchline)
+      
+  } catch (e) {
+      console.log(e, 'oops something went wrong...');
+  }
+}
+function updateElement(elementId, tag,text){
+  const element = document.getElementById(elementId)
+  if(element){
+    element.textContent = text
+  }else{
+createElement(tag,elementId,text)
+  }
+}
+function createElement(tag,elementId, text){
+const element = document.createElement(tag)
+element.id = elementId
+element.textContent = text
+document.querySelector('.Jokebox').appendChild(element)
+}
